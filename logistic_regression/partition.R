07433ee0e456d0cf51c2c3cf9b798473184a8e0f
Logan Harris <lharris4211998@gmail.com>
2022-11-18 16:18:30 -0600

    changes to try and run on hpc

---
+++ logistic_regression/partition.R
@@ -0,0 +1,26 @@
+### Partition and save the data temporarily
+load("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/data/full_data_2.rds")
+score <- mod_dat %>% select_at(vars(CHF:Depression)) %>% rowSums()
+full_data <- mod_dat %>%
+  select(proc, smoker, age, sex, carrier) %>%
+  mutate(score = scale(score))
+
+rm("mod_dat")
+
+npart <- 50
+partitions <- (sample(1:nrow(full_data), replace = FALSE) %% npart) + 1
+
+for (i in 1:npart) {
+  
+  y0 <- as.numeric(unlist(full_data[partitions == i,"proc"]))
+  x0 <- as.matrix(full_data)[partitions == i, -1]
+  nrep0 <- length(y0)
+  name0 <- glue("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partitions/partition{i}.rds")
+  save(
+    y0,
+    x0,
+    nrep0,
+    file = name0
+  )
+  
+}
--- logistic_regression/submit_stochastic_logistic_regression.R
+++ logistic_regression/submit_stochastic_logistic_regression.R
@@ -1,44 +1,52 @@
 library(rstan)
 library(glue)
+library(dplyr)
+
+## clean up folder to store partitions in
+system("rm /Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partitions/*", intern = FALSE)
+system("rm /Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/results/*", intern = FALSE)
 
 ### Will want to partition and submit here
 ### Also include combining data in this file
 
-## May need to likewise submit this
+## Submit the partitioning script
+sub_command <- glue(
+  "#!/bin/bash 
+  qsub -pe smp -2 -cwd -e /dev/null -o /dev/null /Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partition.job"
+)
 
-### Partition and save the data temporarily
-npart <- 20
+system(sub_command, intern = FALSE)
 
-load("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/data/full_data_2.rds")
-full_data <- mod_dat
-rm("mod_dat")
+continue <- length(list.files("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partitions/")) < npart
+i <- 0
 
-partitions <- (sample(1:nrow(full_data), replace = FALSE) %% npart) + 1
-
-for (i in 1:npart) {
+while (continue & i < 100) {
   
-  y0 <- as.numeric(unlist(full_data[partitions == i,"proc"]))
-  x0 <- as.matrix(full_data)[partitions == i, -"proc"]
-  nrep0 <- length(y)
-  name0 <- glue("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partitions/partition{i}.rds")
-  save(
-    y0,
-    x0,
-    nrep0,
-    file = name
-  )
+  Sys.sleep(10)
+  i <- 1 + 1
+  continue <- length(list.files("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/partitions/")) < npary
   
 }
 
 ## submit command
 sub_command <- glue(
   "#!/bin/bash 
-  qsub -pe smp -2 -cwd -e /dev/null -o /dev/null -t 1-{npart} mh.job"
+  qsub -pe smp -2 -cwd -e /dev/null -o /dev/null -t 1-{npart} /Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/slr.job"
 )
 
 system(sub_command, intern = FALSE)
 
 ## Check every minute if all the files are there before combining
+continue <- length(list.files("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/results/")) < npart
+i <- 0
+
+while (continue & i < 100) {
+  
+  Sys.sleep(10)
+  i <- 1 + 1
+  continue <- length(list.files("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/results/")) < npary
+  
+}
 
 
 #######
