d6d8cde0a59808b9ab5823dd62a71f6e63a37c4a
Logan Harris <lharris4211998@gmail.com>
2022-12-01 21:59:11 -0600

    fix file path

--- logistic_regression/mh_dnc_timing.R
+++ logistic_regression/mh_dnc_timing.R
@@ -123,7 +123,7 @@ for (seed in seeds) {
   
   ## Save the draws
   save(full_data_draws, file = glue("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/results/mh_large/simple_{seed}.rds"))
-  save(bary_res, file = glue("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/times/results/wasp_{seed}.rds"))
+  save(bary_res, file = glue("/Shared/Statepi_Marketscan/aa_lh_bayes/bayesian_final_proj/logistic_regression/times/results/mh_large/wasp_{seed}.rds"))
 
 }
 
